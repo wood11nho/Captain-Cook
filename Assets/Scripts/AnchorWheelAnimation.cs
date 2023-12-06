a20dbc86c88e7c8e202286aae8ca9bcb135ac12b
valentinvale <valionsemen@gmail.com>
2023-12-06 02:59:13 +0200

    fixed capstan moving bug

--- Assets/Scripts/AnchorWheelAnimation.cs
+++ Assets/Scripts/AnchorWheelAnimation.cs
@@ -12,19 +12,24 @@ public class AnchorWheelAnimation : MonoBehaviour, IUsable
     public GameObject anchor;
     public AudioClip newTrack;
     private AudioManager audioManager;
+    private int ignoreRaycastLayerMaskInt;
 
     void Start()
     {
         audioManager = FindObjectOfType<AudioManager>();
+        mainCamera.enabled = true;
+        anchorCamera.enabled = false;
+        ignoreRaycastLayerMaskInt = LayerMask.NameToLayer("IgnoreRaycast");
     }
 
     public void Use(GameObject player)
     {
         startGame = !startGame;
         Debug.Log(startGame);
-        gameObject.GetComponent<Animator>().SetBool("startGame", startGame);
+        gameObject.GetComponent<Animator>().SetBool("isMoving", startGame);
         //change the background music
         audioManager.changeBackgroundMusic(newTrack);
+        gameObject.layer = ignoreRaycastLayerMaskInt;
 
         // Trigger the coroutine for switching cameras with a delay
         StartCoroutine(SwitchCamerasWithDelay());
@@ -37,28 +42,28 @@ public class AnchorWheelAnimation : MonoBehaviour, IUsable
     {
         // Wait for 2 seconds
         yield return new WaitForSeconds(2f);
+        gameObject.GetComponent<Animator>().SetBool("isMoving", false);
 
         // Deactivate cameras immediately
-        deactivateAllCams();
+        //deactivateAllCams();
 
         // Switch cameras after the delay
         mainCamera.enabled = !mainCamera.enabled;
         anchorCamera.enabled = !anchorCamera.enabled;
 
         // Anchor animation
-        anchor.GetComponent<Animator>().SetBool("startGame", startGame);
+        anchor.GetComponent<Animator>().SetBool("anchorMoving", startGame);
 
         // Wait for another 3 seconds before switching back to the main camera
         yield return new WaitForSeconds(3f);
 
         // Switch back to the main camera
-        deactivateAllCams();
+        //deactivateAllCams();
         anchorCamera.enabled = !anchorCamera.enabled;
         mainCamera.enabled = !mainCamera.enabled;
 
         // Anchor animation
-        anchor.GetComponent<Animator>().SetBool("startGame", startGame);
-        gameObject.GetComponent<Animator>().SetBool("startGame", false);
+        anchor.GetComponent<Animator>().SetBool("anchorMoving", false);
 
 
 
