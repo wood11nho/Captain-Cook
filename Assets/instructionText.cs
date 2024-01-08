eb2b621359e19d0bc4f99d01f3ec1fb4aa5f9951
Iulita Alpetri <92823812+iulitaalpetri@users.noreply.github.com>
2024-01-08 19:17:32 +0200

    Create instruction guide (#24)
    * add table
    
    * feature: add instruction table and instruction pages
    
    * feature: add intruction pages

---
+++ Assets/instructionText.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class instructionText: MonoBehaviour
+{
+    [SerializeField]
+    private List<Renderer> renderers;
+
+    private List<Material> materials;
+
+    [SerializeField]
+    private TextMeshProUGUI promtText;
+
+    [SerializeField]
+    private Color highlightColor = new Color(0.5f, 0.5f, 0.5f, 0.25f);
+
+    [SerializeField]
+    private Color originalColor;
+
+    // Gets all materials for each renderer on awake
+    private void Awake()
+    {
+        materials = new List<Material>();
+        foreach (Renderer renderer in renderers)
+        {
+            materials.AddRange(new List<Material>(renderer.materials));
+        }
+
+        if (renderers.Count > 0)
+        {
+            originalColor = renderers[0].material.color;
+        }
+    }
+
+    public void ToggleHighlght(bool value)
+    {
+        if (value)
+        {
+            foreach (Material material in materials)
+            {
+                material.EnableKeyword("_EMISSION");
+                material.SetColor("_EmissionColor", originalColor * highlightColor);
+            }
+            ShowPromptMessage("Press E to view instructions"); // Call to display the message
+        }
+        else
+        {
+            foreach (Material material in materials)
+            {
+                material.DisableKeyword("_EMISSION");
+            }
+            ShowPromptMessage(""); // Optionally clear the message
+        }
+    }
+    public void ShowPromptMessage(string text)
+    {
+        if (promtText != null)
+        {
+            promtText.text = text;
+        } 
+    }
+
+}
